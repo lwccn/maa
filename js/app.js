4ff942f85fafcab78702d358a1bf374baf37c8e6
Luke <lu_wu@t-online.de>
2020-07-16 16:25:43 +0200

    hamburger

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 const container = document.querySelector(".container")
 const coffees = [
-  { name: "Perspiciatis", image: "images/coffee1.jpg" },
-  { name: "Voluptatem", image: "images/coffee2.jpg" },
-  { name: "Explicabo", image: "images/coffee3.jpg" },
-  { name: "Rchitecto", image: "images/coffee4.jpg" },
-  { name: " Beatae", image: "images/coffee5.jpg" },
-  { name: " Vitae", image: "images/coffee6.jpg" },
-  { name: "Inventore", image: "images/coffee7.jpg" },
-  { name: "Veritatis", image: "images/coffee8.jpg" },
-  { name: "Accusantium", image: "images/coffee9.jpg" },
+  { name: "01", image: "images/thumbs/01.jpg" },
+  { name: "02", image: "images/thumbs/02.jpg" },
+  { name: "03", image: "images/thumbs/03.jpg" },
+  { name: "04", image: "images/thumbs/04.jpg" },
+  { name: " 05", image: "images/thumbs/05.jpg" },
+  { name: " 06", image: "mages/thumbs/06.jpg" },
+  { name: "07", image: "mages/thumbs/07.jpg" },
+  { name: "avatar", image: "mages/thumbs/avatar.jpg" },
+  { name: "bg", image: "mages/thumbs/bg.jpg" },
 ]
 
 const showCoffees = () => {
@@ -50,4 +50,15 @@ function carousel() {
   if (slideIndex > x.length) {slideIndex = 1}
   x[slideIndex-1].style.display = "block";
   setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
